0db4712f3d66bea9344f698d130fb5fa0fb8e308
jasdox <jason.d.cox07@gmail.com>
2025-05-01 14:26:05 -0400

    Added ability to delete task in task page

--- lib/task_page.dart
+++ lib/task_page.dart
@@ -11,6 +11,7 @@ class TaskPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var pageSelection = Provider.of<PageSelector>(context, listen: false);
+    var taskManager = Provider.of<TaskManager>(context, listen: false);
 
 
     return Scaffold(
@@ -18,20 +19,38 @@ class TaskPage extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(task.title),
       ),
-      body: Column(
-        children: [
-          Text(task.title),
-          Text(task.description ?? ''),
-          Text(task.dueDate.toString()),
-          SizedBox(height: 32,),
-          ElevatedButton(
-                  onPressed: () {
-                      pageSelection.changePage(0);
-                  },
-                  child: Text('Back'),
-                ),
-
-        ],
+      body: Align(
+        alignment: Alignment(0.0, -1),
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              Text("Description: ${task.description ?? ''}"),
+              SizedBox(height: 8,),
+              Text("Due Date: ${task.dueDate.day}/${task.dueDate.month}/${task.dueDate.year}"),
+              SizedBox(height: 32,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                          onPressed: () {
+                              pageSelection.changePage(0);
+                          },
+                          child: Text('Back'),
+                        ),
+                  ElevatedButton(
+                          onPressed: () {
+                              taskManager.removeTask(task);
+                              pageSelection.changePage(0);
+                          },
+                          child: Text('Delete'),
+                        ),
+                ],
+              ),
+          
+            ],
+          ),
+        ),
       ),
       
     );
